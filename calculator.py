fbb29e2ee09fa9862d4f1f3ddc18dfaa11660024
Daniël <daniel.van.der.zee@futurefacts.nl>
2018-03-15 09:52:28 +0100

    Sorry

--- calculator.py
+++ calculator.py
@@ -16,14 +16,14 @@ def multiply(x, y):
 def divide(x, y):
    return x / y
 
-print("Select operation.")
+print("Select operation. ")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
 
 # Take input from the user
-choice = input("Enter choice(1/2/3/4):")
+choice = int(input("Enter choice(1/2/3/4):"))
 
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
